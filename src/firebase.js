c91cc1a92e4912771c3880aa7d378540afba5569
Mittranj <mpansuriya@unomaha.edu>
2020-10-02 14:07:50 -0500

    inital firebase config file

---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyA8r6KO-grt0JdpotD5PFl3s_LUMF9YOm4",
+  authDomain: "clone-react-3781a.firebaseapp.com",
+  databaseURL: "https://clone-react-3781a.firebaseio.com",
+  projectId: "clone-react-3781a",
+  storageBucket: "clone-react-3781a.appspot.com",
+  messagingSenderId: "395509538729",
+  appId: "1:395509538729:web:f1581bc8cce8b60f136eb8",
+  measurementId: "G-PP7969CVH9",
+};
